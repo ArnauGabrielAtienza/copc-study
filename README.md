4195d5c112ff1bb295dd4755b0c4adeaab9781f8
Arnau Gabriel Atienza <arnau12321@gmail.com>
2022-07-19 11:32:06 +0200

    Added worker balance
    Added worker balance and improved documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # copc-testing
-A small repo with code I have developed while studying the COPC Lidar File Format.
+A small repo with code I have developed while studying the COPC Lidar File Format. 
+
+cloudAPI.py contains the CopcHelper class, which provides some functionalities to help reading a COPC file stored
+in the cloud using the laspy library. We take advantage of range downloads to avoid downloading the whole file.
+All those downloads are stored in a local file to avoid repeating the same download.
--- cloudAPI.py
+++ cloudAPI.py
@@ -2,23 +2,28 @@ import laspy.copc as lasp
 from concurrent.futures import ThreadPoolExecutor
 from typing import Tuple
 
-#CopcHelper helps in the process of reading a COPC file using laspy.
-#The files are stored in the cloud and we take advantage of range downloads.
-#Params:
-#   bucket:     name of the bucket where the file is stored
-#   key:        file key in the object storage
-#   filename:   file name for the file that will  be stored locally
-#   s3:         boto3 with access to the cloud where the file is located
 class CopcHelper:
-    #Initialize Reader.
+    """
+    The CopcHelper class provides some functionalities to help reading a COPC file stored
+    in the cloud using the laspy library. We take advantage of range downloads to avoid
+    downloading the whole file.
+    
+    Args:
+        bucket (str): Storage bucket where the file is stored
+        key (str): File key inside the bucket
+        filename (str): Name that will be given to the local file
+        s3: boto3 instance with access to the Object Storage
+    """
     def __init__(self, bucket: str, key: str, filename: str, s3):
         self.bucket = bucket
         self.key = key
         self.filename = filename
         self.ibm_cos = s3
         
-    #Download file header and save it into a file. This function should be one of the first to be called.
     def load_header(self):
+        """Download the file header and store it in a local file. This function should be one of the first
+        to be called.
+        """
         #TODO Change the range to the exact one. It is currently an aproximation.
         header_request = self.ibm_cos.get_object(Bucket=self.bucket, Key=self.key, Range='bytes={}-{}'.format(0, 1024))
         body = header_request['Body']
@@ -27,8 +32,12 @@ class CopcHelper:
             f.write(i)
         f.close()
 
-    #Read the location and size of the rootpage.
     def rootpage_location(self) -> Tuple[int, int]:
+        """Get the location and size of the octree list. The header needs to be loaded first.
+
+        Returns:
+            Tuple[int, int]: Root Offset and Root Size
+        """
         f = open(self.filename, "r+b")
         reader = lasp.CopcReader(f)
         root_offset = reader.copc_info.hierarchy_root_offset
@@ -36,9 +45,9 @@ class CopcHelper:
         f.close()
         return root_offset, root_size
 
-    #Load the rootpage into the main file to be able to read the keys from the octree
-    #and their node entries. 
     def load_rootpage(self):
+        """Download the rootpage/octree and store it in the local file.
+        """
         rootpage = self.rootpage_location()
         root_offset = rootpage[0]
         root_size = rootpage[1]
@@ -53,17 +62,55 @@ class CopcHelper:
         for i in body:
             f.write(i)
         f.close()
+     
+    def get_point_count(self) -> int:
+        """Returns the total amount of points in the file. Only the header needs to be loaded.
 
-    #Get all existing keys.    
+        Returns:
+            int: Number of points
+        """
+        f = open(self.filename, "r+b")
+        reader = lasp.CopcReader(f)
+        points = reader.header.point_count
+        f.close()
+        return points
+   
     def get_all_keys(self) -> list[lasp.VoxelKey]:
+        """Get the keys of all existing nodes in the file.
+
+        Returns:
+            list[lasp.VoxelKey]: List of all keys
+        """
         f = open(self.filename, "r+b")
         reader = lasp.CopcReader(f)
         keys = reader.root_page.entries.keys()
         f.close()
         return keys
+    
+    def get_all_nodes(self) -> list[lasp.Entry]:
+        """Get all the existing nodes (only their metadata).
 
-    #Get the information of all nodes in a given level
-    def get_level_nodes(self, level: lasp.VoxelKey) -> list[lasp.Entry]:
+        Returns:
+            list[lasp.Entry]: List of all nodes
+        """
+        f = open(self.filename, "r+b")
+        reader = lasp.CopcReader(f)
+        keys = reader.root_page.entries.keys()
+        f.close()
+        nodes = []
+        for key in keys:
+            nodes.append(reader.root_page.entries.get(key))
+        return nodes
+
+    def get_level_nodes(self, level: int) -> list[lasp.Entry]:
+        """Get all the existing nodes in a given level (only their metadata).
+
+        Args:
+            level (int): Depth level
+
+        Returns:
+            list[lasp.Entry]: List of existing nodes in level
+        """
         #Get the keys of the nodes in that level
         keys = self.get_all_keys()
         node_keys = []
@@ -80,24 +127,62 @@ class CopcHelper:
         f.close()
         return nodes
     
-    #Get information of a single node
+    def get_level_keys(self, level: int) -> list[lasp.VoxelKey]:
+        """Get all the existing node keys in a given level.
+
+        Args:
+            level (int): Depth level
+
+        Returns:
+            list[lasp.Entry]: List of existing node keys in level
+        """
+        #Get the keys of the nodes in that level
+        keys = self.get_all_keys()
+        node_keys = []
+        for key in keys:
+            if(key.level==level):
+                node_keys.append(key)
+        return node_keys
+    
     def get_node(self, key: lasp.VoxelKey) -> lasp.Entry:
+        """Get the metadata of a node given its key
+
+        Args:
+            key (lasp.VoxelKey): Node key
+
+        Returns:
+            lasp.Entry: Node information
+        """
         f = open(self.filename, "r+b")
         reader = lasp.CopcReader(f)
         node = reader.root_page.entries.get(key)
         f.close()
         return node
-    
-    #Get the parent of a node given its key   
+     
     def get_parent(self, key: lasp.VoxelKey) -> lasp.VoxelKey:
+        """Get the theoretical parent key, even though it may not exist in the octree.
+
+        Args:
+            key (lasp.VoxelKey): Child key
+
+        Returns:
+            lasp.VoxelKey: Parent key
+        """
         parent = lasp.VoxelKey()
         parent.x = int((key.x)/2)
         parent.y = int((key.y)/2)
         parent.level = key.level - 1
         return parent
     
-    #Get all children of a given node.
     def get_children(self, parentkey: lasp.VoxelKey) -> list[lasp.VoxelKey]:
+        """Get all existing children of a node given its key.
+
+        Args:
+            parentkey (lasp.VoxelKey): Parent key
+
+        Returns:
+            list[lasp.VoxelKey]: List of child keys
+        """
         keys = []
         
         x = parentkey.x
@@ -143,17 +228,23 @@ class CopcHelper:
                 keys.append(key)
             
         return keys
-    
-    #Download and load the points of a set of nodes
-    def multiple_points_download(self, keys: lasp.VoxelKey):
+
+    def multiple_points_download(self, nodes: list[lasp.Entry]):
+        """Download and load into file multiple nodes (their points) at the same time using threads.
+
+        Args:
+            nodes (list[lasp.Entry]): Nodes to be loaded
+        """
         with ThreadPoolExecutor() as executor:
-            executor.map(self.load_points, keys)
+            executor.map(self.load_points, nodes)
     
-    #Download a set of points in a node given its key. The load them into the local file
-    #to be able to get them
-    def load_points(self, key: lasp.VoxelKey):
+    def load_points(self, node: lasp.Entry):
+        """Download a set of points in a node and load the into the local file.
+
+        Args:
+            node (lasp.Entry): Node to be downloaded
+        """
         #Download the node
-        node = self.get_node(key)
         point_request = self.ibm_cos.get_object(Bucket=self.bucket, Key=self.key, Range='bytes={}-{}'.format(node.offset, node.offset + node.byte_size))
         body = point_request['Body']
         
@@ -163,116 +254,77 @@ class CopcHelper:
         for i in body:
             f.write(i)
         f.close()
-    
-    #Retrieve points from a node given its key. The node should be loaded into memory first.    
-    def get_points(self, key: lasp.VoxelKey) -> lasp.ScaleAwarePointRecord:
+      
+    def get_points(self, nodes: list[lasp.Entry]) -> lasp.ScaleAwarePointRecord:
+        """Retrieve the points of a list of nodes. The should be downloaded first 
+        using multiple_points_download() or load_points().
+
+        Args:
+            nodes (list[lasp.Entry]): Nodes to be retrieved
+
+        Returns:
+            lasp.ScaleAwarePointRecord: List of points
+        """
         f = open(self.filename, "r+b")
         reader = lasp.CopcReader(f)
-        points = reader._fetch_and_decrompress_points_of_nodes([reader.root_page.entries.get(key)])
+        points = reader._fetch_and_decrompress_points_of_nodes(nodes)
         f.close()
         return points
     
-    #Get the neighbours of a given node at the deepest level (4).
-    def get_smallest_neighbours(self, key: lasp.VoxelKey) -> list[lasp.VoxelKey]:
-        neighbours = []
+    def point_balance(self, points: int) -> list[list[lasp.Entry]]:
+        """Split the nodes into different groups. Each group will have a similar amount of points.
+        This allows better balance if we want to split the file between different workers.
+
+        Args:
+            points (int): Amount of points per worker
+
+        Returns:
+            list[list[lasp.Entry]]: List with the node list that each worker should compute
+        """
+        balance = []
         
-        all_keys = list(self.get_all_keys())
-        x = key.x
-        y = key.y
-        z = key.z
-        level = key.level
+        keys = self.get_all_nodes()
         
-        num = 2 ** (4-level)    #Number of level 4 nodes in our node / 2. Helpful in a lot of calculations.
-        
-        #In this first part we search for the nodes at the sides and their up and down version.
-        #These nodes are the ones that are adjacent but are not immediately up or down of our node.
-        for k in range(num+2):
-            #Nodes at the back
-            if(y!=-1):
-                for i in range(num + 1):
-                    nkey = lasp.VoxelKey()
-                    nkey.level = 4
-                    nkey.y = y*num - 1
-                    nkey.z = z + k - 1
-                    nkey.x = x*num + i
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
+        worker = []
+        num_points = 0
+        for node in keys:
+            num_points += node.point_count
+            worker.append(node)
+            if num_points > points:
+                num_points = 0
+                balance.append(worker)
+                worker = []
                 
-            #Nodes at the left
-            if(x!=-1):
-                for i in range(num + 1):
-                    nkey = lasp.VoxelKey()
-                    nkey.level = 4
-                    nkey.y = y*num + i - 1
-                    nkey.z = z + k - 1
-                    nkey.x = x*num - 1
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
-                    
-            #Nodes at the front
-            max = (2 ** level) - 1
-            if(y!=max):
-                for i in range(num + 1):
-                    nkey = lasp.VoxelKey()
-                    nkey.level = 4
-                    nkey.y = y*num + num
-                    nkey.z = z + k - 1
-                    nkey.x = x*num + i - 1
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
-                    
-            #Nodes at the right
-            if(x!=max):
-                for i in range(num + 1):
-                    nkey = lasp.VoxelKey()
-                    nkey.level = 4
-                    nkey.y = y*num + i
-                    nkey.z = z + k - 1
-                    nkey.x = x*num + num
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
+        if num_points > 0:
+            balance.append(worker)
         
-        #Now we need to find the nodes above and below our node. In the case that we are already at level 4
-        #we just need the node above/below us. To do this we calculate the height that they should have and
-        #then we 
-        #Up
-        if level==4:
-            upkey = lasp.VoxelKey()
-            upkey.x = x
-            upkey.y = y
-            upkey.z = z+1
-            upkey.level = 4
-            if nkey in all_keys:
-                neighbours.append(nkey)
-        else:
-            for i in range(num):
-                for j in range(num):
-                    nkey = lasp.VoxelKey()
-                    nkey.x = x + i
-                    nkey.y = y + j
-                    nkey.z = z+num
-                    nkey.level = 4
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
+        return balance
+    
+    def size_balance(self, size: int) -> list[list[lasp.Entry]]:
+        """Split the nodes into different groups. Each group will have a similar download size.
+        This allows better balance if we want to split the file between different workers.
+
+        Args:
+            size (int): Download size per worker
+
+        Returns:
+            list[list[lasp.Entry]]: List with the node list that each worker should compute
+        """
+        balance = []
         
-        #Down
-        if level==4:
-            upkey = lasp.VoxelKey()
-            upkey.x = x
-            upkey.y = y
-            upkey.z = z-1
-            upkey.level = 4
-            if nkey in all_keys:
-                neighbours.append(nkey)
-        else:
-            for i in range(num):
-                for j in range(num):
-                    nkey = lasp.VoxelKey()
-                    nkey.x = x + i
-                    nkey.y = y + j
-                    nkey.z = z*num - 1
-                    nkey.level = 4
-                    if nkey in all_keys:
-                        neighbours.append(nkey)
-                        
-        return neighbours
+        keys = self.get_all_nodes()
+        
+        worker = []
+        num_size = 0
+        for node in keys:
+            num_size += node.byte_size
+            worker.append(node)
+            if num_size > size:
+                num_size = 0
+                balance.append(worker)
+                worker = []
+                
+        if num_size > 0:
+            balance.append(worker)
+        
+        return balance
